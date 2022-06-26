9816a66d7c7ceb894c92b4e168806df59e7dde02
Salavat F <salmrproperfahr@mail.ru>
2022-06-26 20:58:21 +0300

    1.6.8

---
+++ 1-6.8/script.js
@@ -0,0 +1,23 @@
+// 1 setInterval
+function printNumbers(from, to) {
+    let current = from;
+    let timerId = setInterval(function() {
+      alert(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+}
+// 2 setTimeout
+function printNumbers(from, to) {
+    let current = from;
+  
+    setTimeout(function go() {
+      alert(current);
+      if (current < to) {
+        setTimeout(go, 1000);
+      }
+      current++;
+    }, 1000);
+}
