4f7df4fe2b37e6affed8a6a6f5117e9b6cf591f2
Salavat F <salmrproperfahr@mail.ru>
2022-06-27 14:41:27 +0300

    2.1.8

---
+++ 2-1.8/script.js
@@ -0,0 +1,16 @@
+function showNotification({top = 0, right = 0, className, html}) {
+
+    let notification = document.createElement('div');
+    notification.className = "notification";
+    if (className) {
+      notification.classList.add(className);
+    }
+
+    notification.style.top = top + 'px';
+    notification.style.right = right + 'px';
+
+    notification.innerHTML = html;
+    document.body.append(notification);
+
+    setTimeout(() => notification.remove(), 1500);
+}
